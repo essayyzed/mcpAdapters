65d225f0d39ab05ab98ed693298d0125e6c9c60c
Asad <essayyzed@gmail.com>
2025-07-07 17:53:44 +0500

    Enhance main.py with ChatOpenAI integration and StdioServerParameters setup; add isort dependency in pyproject.toml; update weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import StdioServerParameters
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY")) 
+ 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command = "python",
+    args = ["/Users/essayyzed/temp/mcp-server/mcpAdapters/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcpadapters!")
